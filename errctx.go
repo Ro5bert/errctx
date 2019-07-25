a83c978cfa29a2e3b6f841ac2d97b848a4d9d2c5
robert <robertrussell.72001@gmail.com>
2019-07-25 12:46:40 -0700

    Add documentation
    All exported functions, the Error interface, and the errctx package
    are now documented.

--- errctx.go
+++ errctx.go
@@ -1,3 +1,25 @@
+// Package errctx implements a simple method of adding context (in the form of strings) to errors, as one might want to
+// do as an error propagates back through a long call chain.
+//
+// errctx provides the Error interface, which embeds the standard error interface to produce error strings of the
+// following form:
+//
+//     <third piece of context>: <second piece of context>: <first piece of context>: <root error message>
+//
+// (The angle brackets are not necessarily present; they exist to represent that they and the enclosed text would be
+// replaced by the string described by the enclosed text. Obviously, one can have an (almost) arbitrary number of
+// context strings.)
+//
+// Each context string in an Error is added to the Error via WithCtx or WithCtxf. Alternatively, context strings may be
+// added directly to Errors via Error.AddCtx or Error.AddCtxf.
+//
+// The ordinal adjectives in the example error message refer to the order the context strings are added to an Error. So
+// the "first" context string added to an Error appears closest to the root error, the "second" context string the
+// second closest, etc.
+//
+// When an Error is implicitly created via WithCtx or WithCtxf, the given error's error string is used as the "root
+// error message". When an Error is explicitly created via New or Newf, the given string is used as the "root error
+// message".
 package errctx
 
 import (
@@ -8,6 +30,10 @@ import (
 
 const joinSequence = ":"
 
+// TODO: strip context strings as soon as they're added
+
+// Root returns the "root" error for the given error, err. If err implements Error, then Root returns err.Root();
+// otherwise, Root simply returns err.
 func Root(err error) error {
 	if err, ok := err.(Error); ok {
 		return err.Root()
@@ -15,6 +41,9 @@ func Root(err error) error {
 	return err
 }
 
+// WithCtx adds the given context string to the given error, err. If err implements Error, then WithCtx adds the context
+// string to err (as if via err.AddCtx) and returns err; otherwise, WithCtx constructs a new Error, adds the context
+// string to it, then returns it.
 func WithCtx(err error, ctx string) Error {
 	if err, ok := err.(Error); ok {
 		err.AddCtx(ctx)
@@ -23,25 +52,40 @@ func WithCtx(err error, ctx string) Error {
 	return &ctxerror{err, []string{ctx}}
 }
 
+// WithCtxf adds the context string derived from the given format specifier to the given error, err. If err implements
+// Error, then WithCtxf adds the context string to err (as if via err.AddCtx) and returns err; otherwise, WithCtx
+// constructs a new Error, adds the context string to it, then returns it.
 func WithCtxf(err error, format string, a ...interface{}) Error {
 	return WithCtx(err, fmt.Sprintf(format, a...))
 }
 
+// New constructs a new Error with no context and a root error with the given text.
 func New(text string) Error {
 	return &ctxerror{root: errors.New(text)}
 }
 
+// Newf constructs a new Error with no context and a root error with text derived from the given format specifier.
 func Newf(format string, a ...interface{}) Error {
 	return New(fmt.Sprintf(format, a...))
 }
 
+// Error is an interface that embeds the standard error interface. Implementors of Error return a formatted error string
+// from the Error method that agrees with the package documentation.
 type Error interface {
 	error
+
+	// Root returns the "root" error of this Error; that is, the error on to which all the context strings are
+	// prepended.
 	Root() error
+
+	// AddCtx adds the given context string to this Error.
 	AddCtx(ctx string)
+
+	// AddCtxf adds the context string derived from the given format specifier to this Error.
 	AddCtxf(format string, a ...interface{})
 }
 
+// ctxerror is the default implementation of Error.
 type ctxerror struct {
 	root error
 	ctx  []string
@@ -65,8 +109,8 @@ func (err ctxerror) Error() string {
 	builder.WriteString(last)
 	for i := len(err.ctx) - 2; i >= 0; i-- {
 		trimmedCtx := strings.TrimSpace(err.ctx[i])
-        writeCtxToBuilder(&builder, last, trimmedCtx)
-        last = trimmedCtx
+		writeCtxToBuilder(&builder, last, trimmedCtx)
+		last = trimmedCtx
 	}
 	writeCtxToBuilder(&builder, last, err.root.Error())
 	return builder.String()
